86d0bf91edf651861c4effc6433faa9a8dce2206
imaciasm <imaciasm@uoc.edu>
2021-01-06 13:33:59 +0100

    documentation in tests

--- tests/tests.py
+++ tests/tests.py
@@ -11,17 +11,29 @@ ficheros = FICHEROS
 class TestAnalisisBaciloKoch(unittest.TestCase):
 
     def test_1_1(self):
+        """
+        Funcion para testear ejercicio 1.1 funcion count_clases_orfs
+        :return: None
+        """
         golden_number = 3
         result, orfs, clases, descriptions = count_clases_orfs(ficheros)
         self.assertEqual(result['2220'], golden_number)
 
     def test_1_2(self):
+        """
+        Funcion para testear ejercicio 1.2 funcion count_clases_orfs
+        :return: None
+        """
         result, orfs, clases, descriptions = count_clases_orfs(ficheros)
         frq, clase = freq_clase("Pentose phosphate pathway ", result, ficheros)
         self.assertEqual(frq, 11)
         self.assertEqual(clase, '1250')
 
     def test_2_1(self):
+        """
+        Funcion para testear ejercicio 2.1 funcion clases_listado
+        :return: None
+        """
         result, orfs, clases, descriptions = count_clases_orfs(ficheros)
         listado = ["electron"]
         clases_escogidas, orfs_escogidos = clases_listado(clases, orfs, descriptions, listado)
@@ -29,11 +41,19 @@ class TestAnalisisBaciloKoch(unittest.TestCase):
         self.assertEqual(orfs_escogidos['electron'], {'tb3029', 'tb3028', 'tb3053'})
 
     def test_2_2(self):
+        """
+        Funcion para testear ejercicio 2.2 funcion promedio_orf
+        :return: None
+        """
         orfs_escogidos = {"electron": ['tb3029']}
         resultado = promedio_orf(orfs_escogidos)
         self.assertEqual(resultado['electron'], 21)
 
     def test_3(self):
+        """
+        Funcion para testear ejercicio 3 funcion count_clases_orfs
+        :return: None
+        """
         result, orfs, clases, descriptions = count_clases_orfs(ficheros)
         aptos = multiple(clases)
         self.assertEqual(aptos[9], 3)
